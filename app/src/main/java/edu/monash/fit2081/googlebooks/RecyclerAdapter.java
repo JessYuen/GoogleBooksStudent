d4c789f1cead72bd7f08becbad4cb9fb95a85af3
Dag-ner <jess986.me@gmail.com>
2020-01-09 05:21:25 +1100

    color

--- app/src/main/java/edu/monash/fit2081/googlebooks/RecyclerAdapter.java
+++ app/src/main/java/edu/monash/fit2081/googlebooks/RecyclerAdapter.java
@@ -1,13 +1,11 @@
 package edu.monash.fit2081.googlebooks;
 
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
