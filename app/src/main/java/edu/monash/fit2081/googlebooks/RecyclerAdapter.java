2b6d63c84e81a0e54f350d5be235ed4cc6ba01c9
jessicayuen <jess986.me@gmail.com>
2020-01-08 19:13:03 +1100

    Initial commit

---
+++ app/src/main/java/edu/monash/fit2081/googlebooks/RecyclerAdapter.java
@@ -0,0 +1,70 @@
+package edu.monash.fit2081.googlebooks;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private final ArrayList<GoogleBook> bookList;
+
+    public RecyclerAdapter(ArrayList<GoogleBook> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout, viewGroup, false); //CardView inflated as RecyclerView list item
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        GoogleBook currentItem = bookList.get(position);
+
+        viewHolder.tvTitle.setText(currentItem.getBookTitle());
+        viewHolder.tvAuthor.setText(currentItem.getAuthors());
+        viewHolder.tvYear.setText(currentItem.getPublishedDate());
+
+        //a class declared in a method (so called local or anonymous class can only access the method's local variables if they are declared final (1.8 or are effectively final)
+        //this has to do with Java closures
+        // see https://docs.oracle.com/javase/tutorial/java/javaOO/localclasses.html and https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html
+        final int fPosition = position;
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() { //set back to itemView for students
+            @Override public void onClick(View v) {
+
+                Snackbar.make(v, "Item at position " + fPosition + " was clicked!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvTitle, tvAuthor, tvYear;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.tvTitle = itemView.findViewById(R.id.item_title);
+            this.tvAuthor = itemView.findViewById(R.id.item_author);
+            this.tvYear = itemView.findViewById(R.id.item_year);
+
+        }
+    }
+
+
+}
